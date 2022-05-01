43a53d41e048a539c9d11a673a5d3ca17ac886e6
Abderrahmen Chouat <chouatabderrahmen@gmail.com>
2022-04-30 22:52:12 -0700

    <Complete>Build Angular blog w/Scully Jamstack lazyloading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactModule } from './contact/contact.module';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { ScullyLibModule } from '@scullyio/ng-lib';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    CoreModule,
+    SharedModule,
+    ContactModule,
+    ScullyLibModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
